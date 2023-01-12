286b03f43f061e18dfd8bc9ac8e7f005746064e7
Pierre Bertier <pierredelaval@hotmail.fr>
2023-01-12 21:49:19 +0100

    WIP Grid

--- src/App.js
+++ src/App.js
@@ -8,13 +8,23 @@ let wh = window.innerHeight
 const isFirefox = navigator.userAgent.indexOf('Firefox') > -1
 const isWindows = navigator.appVersion.indexOf("Win") != -1
 
-const mouseMultiplier = .6
+const mouseMultiplier = .001
 const firefoxMultiplier = 20
 
 const multipliers = {
   mouse: isWindows ? mouseMultiplier * 2 : mouseMultiplier,
   firefox: isWindows ? firefoxMultiplier * 2 : firefoxMultiplier
 }
+
+
+let bounds = {}
+let spacing = .075
+bounds.smallSize = 1;
+bounds.halfSmallSize = bounds.smallSize / 2;
+bounds.largeSize = (2 * bounds.smallSize) + spacing;
+bounds.halfLargeSize = bounds.largeSize / 2;
+bounds.width = 8 * bounds.smallSize + (7 * spacing);
+bounds.height = 5 * bounds.smallSize + (4 * spacing);
 
 const loader = new THREE.TextureLoader()
 
@@ -28,13 +38,28 @@ varying vec2 vUv;
 void main(){
 vec3 pos = position;
 
-pos.y *= 1. - u_diff;
-pos.x *= 1. - u_diff;
+
 
 vUv = uv;
 gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.);;
 }
 `
+
+// `precision mediump float;
+// #define GLSLIFY 1
+// attribute vec2 uv;
+// attribute vec3 position;
+// uniform mat4 modelViewMatrix;
+// uniform mat4 projectionMatrix;
+// uniform float uTime;
+// varying vec2 vUv;
+// void main() {  
+//   vUv = uv;
+
+//   vec3 pos = position;
+
+//   gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);
+// }`
 
 const fragmentShader = `
 precision mediump float;
@@ -133,13 +158,14 @@ const App = () => {
   var planes = []
 
   class MainMesh extends THREE.Object3D {
-    init(i, position) {
+    init(i, size, position) {
 
       this.i = i
       this.x = 0
       this.y = 0
+      this.size = size
       this.center = { value: 1 }
-      this.spacing = 0
+      this.spacing = spacing
       this.startPosition = position
 
       this.my = 1 - ((i % 5) * 0.1)
@@ -148,7 +174,7 @@ const App = () => {
 
     createPlane() {
       this.plane = new Plane()
-      this.plane.init(this.i)
+      this.plane.init(this.i, this.size)
       this.add(this.plane)
       this.plane.resize()
     }
@@ -156,13 +182,13 @@ const App = () => {
     update = (pos, widthDividedByTwo) => {
 
       this.x = gsap.utils.wrap(
-        -(widthDividedByTwo.x - this.spacing),
+        (-widthDividedByTwo.x - this.spacing),
         widthDividedByTwo.x,
         this.startPosition.x + pos.x + 0
       )
 
       this.y = gsap.utils.wrap(
-        -(widthDividedByTwo.y - this.spacing),
+        (-widthDividedByTwo.y - this.spacing),
         widthDividedByTwo.y,
         this.startPosition.y + pos.y + 0.55
       )
@@ -188,7 +214,7 @@ const App = () => {
   }
 
   class Plane extends THREE.Object3D {
-    init(i) {
+    init(i, size) {
 
       this.x = 0
       this.y = 0
@@ -196,8 +222,9 @@ const App = () => {
 
       this.my = 1 - ((i % 5) * 0.1)
 
+      this.size = size
+      this.geometry = new THREE.PlaneGeometry(size, size)
 
-      this.geometry = geometryPlane
       this.material = planeMaterial.clone()
       this.isInCenter = false
       this.material.uniforms = {
@@ -216,112 +243,193 @@ const App = () => {
 
 
     resize() {
-      this.mesh.scale.set(150, 150, 1)
+
     }
   }
-
-
-  const delay = ms => new Promise(
-    resolve => setTimeout(resolve, ms)
-  );
 
   useEffect(() => {
 
     var scene = new THREE.Scene();
 
-    var camera = new THREE.OrthographicCamera(
-      ww / -2, ww / 2, wh / 2, wh / -2, 1, 1000
+
+    var camera = new THREE.PerspectiveCamera(35, ww / wh, 0.1, 1000
     )
 
+
+    camera.position.set(0, 0, 5)
+
+    console.log(scene.position)
     camera.lookAt(scene.position)
-    camera.position.z = 1
+
 
     var renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true })
     renderer.setSize(ww, wh)
-    renderer.setPixelRatio(
-      gsap.utils.clamp(1, 1.5, window.devicePixelRatio)
-    )
 
     renderer.setClearColor(0xffffff, 0);
     mountRef.current.appendChild(renderer.domElement);
+
 
     addPlanes();
     addEvents();
     addTick()
     resize()
 
+
+
     function addPlanes() {
       planes = []
       scene.clear()
 
-      const plane = new MainMesh()
-      plane.init(1, new THREE.Vector3(0, 0, 0))
-      planes.push(plane)
-      scene.add(plane)
-
-      const plane2 = new MainMesh()
-      plane2.init(2, new THREE.Vector3(170, 0, 0))
-      planes.push(plane2)
-      scene.add(plane2)
-
-      const plane3 = new MainMesh()
-      plane3.init(3, new THREE.Vector3(340, 0, 0))
-      planes.push(plane3)
-      scene.add(plane3)
-
-      const plane4 = new MainMesh()
-      plane4.init(3, new THREE.Vector3(0, 170, 0))
-      planes.push(plane4)
-      scene.add(plane4)
+      // for (let i = 0; i < 12; i++) {
+      //   for (let j = 0; j < 12; j++) {
+      //     const plane = new MainMesh()
+      //     plane.init(i, new THREE.Vector3((170 * i), (170 * j), 0))
+      //     planes.push(plane)
+      //     scene.add(plane)
+      //   }
+      // }
 
 
-      const plane5 = new MainMesh()
-      plane5.init(3, new THREE.Vector3(170, 170, 0))
-      planes.push(plane5)
-      scene.add(plane5)
+      ///////
 
-      const plane6 = new MainMesh()
-      plane6.init(3, new THREE.Vector3(340, 170, 0))
-      planes.push(plane6)
-      scene.add(plane6)
+      ////
 
-      const plane7 = new MainMesh()
-      plane7.init(3, new THREE.Vector3(0, 340, 0))
-      planes.push(plane7)
-      scene.add(plane7)
+      // const plane1 = new MainMesh()
+      // plane1.init(i, new THREE.Vector3((170 * i), (170 * j), 0))
+      // planes.push(plane1)
+      // scene.add(plane1)
 
-      const plane8 = new MainMesh()
-      plane8.init(3, new THREE.Vector3(170, 340, 0))
-      planes.push(plane8)
-      scene.add(plane8)
 
-      const plane9 = new MainMesh()
-      plane9.init(3, new THREE.Vector3(340, 340, 0))
-      planes.push(plane9)
-      scene.add(plane9)
+      let index = 1
+
+      const e = new MainMesh()
+      e.init(index++, bounds.smallSize, new THREE.Vector3(bounds.halfSmallSize, bounds.halfSmallSize, 0))
+      planes.push(e)
+      scene.add(e)
+
+      const n = new MainMesh()
+      n.init(index++, bounds.smallSize, new THREE.Vector3(bounds.halfSmallSize + bounds.smallSize + spacing, bounds.halfSmallSize, 0))
+      planes.push(n)
+      scene.add(n)
+
+      const s = new MainMesh()
+      s.init(index++, bounds.smallSize, new THREE.Vector3(bounds.halfSmallSize + 2 * bounds.smallSize + 2 * spacing, bounds.halfSmallSize, 0))
+      planes.push(s)
+      scene.add(s)
+
+      const i = new MainMesh()
+      i.init(index++, bounds.largeSize, new THREE.Vector3(bounds.halfLargeSize + 3 * bounds.smallSize + 3 * spacing, bounds.halfLargeSize, 0))
+      planes.push(i)
+      scene.add(i)
+
+      const o = new MainMesh()
+      o.init(index++, bounds.smallSize, new THREE.Vector3(bounds.halfSmallSize + 5 * bounds.smallSize + 5 * spacing, bounds.halfSmallSize, 0))
+      planes.push(o)
+      scene.add(o)
+
+      const a = new MainMesh()
+      a.init(index++, bounds.largeSize, new THREE.Vector3(bounds.halfLargeSize + 6 * bounds.smallSize + 6 * spacing, bounds.halfLargeSize, 0))
+      planes.push(a)
+      scene.add(a)
+
+      const u = new MainMesh()
+      u.init(index++, bounds.smallSize, new THREE.Vector3(bounds.halfSmallSize, bounds.halfSmallSize + bounds.smallSize + spacing, 0))
+      planes.push(u)
+      scene.add(u)
+
+      const c = new MainMesh()
+      c.init(index++, bounds.largeSize, new THREE.Vector3(bounds.halfLargeSize + 1 * bounds.smallSize + spacing, bounds.largeSize + .5 * spacing, 0))
+      planes.push(c)
+      scene.add(c)
+
+      const l = new MainMesh()
+      l.init(index++, bounds.smallSize, new THREE.Vector3(bounds.halfSmallSize + 5 * bounds.smallSize + 5 * spacing, bounds.halfSmallSize + bounds.smallSize + spacing, 0))
+      planes.push(l)
+      scene.add(l)
+
+      const d = new MainMesh()
+      d.init(index++, bounds.smallSize, new THREE.Vector3(bounds.halfSmallSize, bounds.halfSmallSize + 2 * bounds.smallSize + 2 * spacing, 0))
+      planes.push(d)
+      scene.add(d)
+
+      const f = new MainMesh()
+      f.init(index++, bounds.smallSize, new THREE.Vector3(bounds.halfSmallSize + 3 * bounds.smallSize + 3 * spacing, bounds.halfSmallSize + 2 * bounds.smallSize + 2 * spacing, 0))
+      planes.push(f)
+      scene.add(f)
+
+      const h = new MainMesh()
+      h.init(index++, bounds.smallSize, new THREE.Vector3(bounds.halfSmallSize + 4 * bounds.smallSize + 4 * spacing, bounds.halfSmallSize + 2 * bounds.smallSize + 2 * spacing, 0))
+      planes.push(h)
+      scene.add(h)
+
+      const m = new MainMesh()
+      m.init(index++, bounds.largeSize, new THREE.Vector3(bounds.halfLargeSize + 5 * bounds.smallSize + 5 * spacing, bounds.largeSize + bounds.halfLargeSize + spacing, 0))
+      planes.push(m)
+      scene.add(m)
+
+      const g = new MainMesh()
+      g.init(index++, bounds.smallSize, new THREE.Vector3(bounds.halfSmallSize + 7 * bounds.smallSize + 7 * spacing, bounds.halfSmallSize + 2 * bounds.smallSize + 2 * spacing, 0))
+      planes.push(g)
+      scene.add(g)
+
+      const v = new MainMesh()
+      v.init(index++, bounds.smallSize, new THREE.Vector3(bounds.halfSmallSize, bounds.halfSmallSize + 3 * bounds.smallSize + 3 * spacing, 0))
+      planes.push(v)
+      scene.add(v)
+
+      const D = new MainMesh()
+      D.init(index++, bounds.smallSize, new THREE.Vector3(bounds.halfSmallSize + 1 * bounds.smallSize + spacing, bounds.halfSmallSize + 3 * bounds.smallSize + 3 * spacing, 0))
+      planes.push(D)
+      scene.add(D)
+
+      const x = new MainMesh()
+      x.init(index++, bounds.smallSize, new THREE.Vector3(bounds.halfSmallSize + 2 * bounds.smallSize + 2 * spacing, bounds.halfSmallSize + 3 * bounds.smallSize + 3 * spacing, 0))
+      planes.push(x)
+      scene.add(x)
+
+      const y = new MainMesh()
+      y.init(index++, bounds.smallSize, new THREE.Vector3(bounds.halfSmallSize + 3 * bounds.smallSize + 3 * spacing, bounds.halfSmallSize + 3 * bounds.smallSize + 3 * spacing, 0))
+      planes.push(y)
+      scene.add(y)
+
+      const w = new MainMesh()
+      w.init(index++, bounds.smallSize, new THREE.Vector3(bounds.halfSmallSize + 4 * bounds.smallSize + 4 * spacing, bounds.halfSmallSize + 3 * bounds.smallSize + 3 * spacing, 0))
+      planes.push(w)
+      scene.add(w)
+
+      const b = new MainMesh()
+      b.init(index++, bounds.smallSize, new THREE.Vector3(bounds.halfSmallSize + 7 * bounds.smallSize + 7 * spacing, bounds.halfSmallSize + 3 * bounds.smallSize + 3 * spacing, 0))
+      planes.push(b)
+      scene.add(b)
+
+      //////
+
+      const C = ["Gudupop", "Charm", "Threads", "Monsoon", "Gates", "Disarray", "Colorcase", "Nacre"];
+      for (let e = 0; e < 8; e++) {
+        let n = 20 + e;
+        const b = new MainMesh()
+        const s = new THREE.Vector3(bounds.halfSmallSize + e * (1 + spacing), bounds.halfSmallSize + 4 * bounds.smallSize + 4 * spacing, 0)
+        b.init(index++, bounds.smallSize, s)
+        planes.push(b)
+        scene.add(b)
+      }
+
+      ///////
 
 
       planes.forEach((t => {
-        t.startPosition.x -= 150 / 2;
-        t.startPosition.y -= 150 / 2;
-        t.position.x -= 150 / 2;
-        t.position.y -= 150 / 2
+        t.startPosition.x -= bounds.width / 2;
+        t.startPosition.y -= bounds.height / 2;
+        t.position.x -= bounds.width / 2;
+        t.position.y -= bounds.height / 2
       }))
-
     }
-
-    let currentOffset = { x: 0, y: 0 }
-    let isOpen = false
 
     function addTick() {
       gsap.ticker.add(tick)
     }
 
     function addEvents() {
-
-
-      // window.addEventListener('tick', tick)
-
       window.addEventListener('mousemove', onMouseMove)
       window.addEventListener('mousedown', onMouseDown)
       window.addEventListener('mouseup', onMouseUp)
@@ -329,10 +437,6 @@ const App = () => {
     }
 
     function removeEvents() {
-
-
-      // window.addEventListener('tick', tick)
-
       window.removeEventListener('mousemove', onMouseMove)
       window.removeEventListener('mousedown', onMouseDown)
       window.removeEventListener('mouseup', onMouseUp)
@@ -349,25 +453,18 @@ const App = () => {
       max.y = bottom
     }
 
-    let clickPos = { x: 0, y: 0 }
-
-
-
-
-    let current = new THREE.Vector3()
     let target = new THREE.Vector3()
     let on = new THREE.Vector3()
     let mouse = new THREE.Vector3()
     let wheel = new THREE.Vector3()
-    let pos = new THREE.Vector3(0, 0)
-    let vel = new THREE.Vector3()
 
     function onMouseMove(t) {
 
       let n = t.clientX;
       let s = t.clientY;
       mouse.x = n / ww * 2 - 1;
-      mouse.y = -s / wh * 2 + 1;
+      mouse.y = -s / wh * 2 + 1
+
 
       if (isDragging) {
         target.set(on.x + 2.5 * n, on.y - 2.5 * s)
@@ -406,6 +503,7 @@ const App = () => {
       target.x += wheel.x;
       target.y -= wheel.y
 
+
     }
 
     function tick() {
@@ -420,7 +518,7 @@ const App = () => {
 
       planes.length
         && planes.forEach(plane =>
-          plane.update({ x: cx, y: cy }, { x: ww / 1.5, y: wh / 1.5 }))
+          plane.update({ x: cx, y: cy }, { x: bounds.width / 2, y: bounds.height / 2 }))
 
       renderer.render(scene, camera)
     }
